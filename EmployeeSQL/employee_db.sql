b82714ffef5c3017fd3f81687941baa11239290f
dkuchan415 <danielkuchan@yahoo.com>
2020-10-26 19:18:37 -0500

    Created all tables and imported csv into table correctly

--- EmployeeSQL/employee_db.sql
+++ EmployeeSQL/employee_db.sql
@@ -3,7 +3,7 @@
 --Create Employee Info table
 
 CREATE TABLE employee_info (
-	emp_no int,
+	emp_no int PRIMARY KEY,
 	emp_title_id varchar(5),
 	birth_date date,
 	first_name varchar(30),
@@ -12,6 +12,12 @@ CREATE TABLE employee_info (
 	hire_date date
 );	
 
+--Select from employee table to make sure data is correct
+
+SELECT * FROM employee_info;
+--Is there a better way to display sex? (M vs. F) possibly as only accepting the two values.
+--Is "date" the right way to identify birthdate and  hiredate?
+
 --Create Employee Salaries table
 
 CREATE TABLE employee_salaries (
@@ -19,12 +25,15 @@ CREATE TABLE employee_salaries (
 	salary money
 );
 
+SELECT *FROM employee_salaries;
 --Create Employee Titles table 
 
 CREATE TABLE employee_titles (
-	emp_no int,
+	title_id varchar(5),
 	title varchar(30)
 );
+
+SELECT * FROM employee_titles;
 
 --Create Employee Department table 
 	
@@ -33,6 +42,8 @@ CREATE TABLE employee_department (
 	dept_no varchar(4)
 );
 
+SELECT * FROM employee_department;
+
 --Create Department table
 
 CREATE TABLE department (
@@ -40,9 +51,15 @@ CREATE TABLE department (
 	dept_name varchar(30)
 );
 
+SELECT * FROM department;
+
+-- Create manager department table
+
 CREATE TABLE manager_department (
-	emp_no int,
-	dept_no varchar(4)
+	dept_no varchar(4),
+	emp_no int
 );
+
+SELECT * FROM manager_department;
 
 --Questions for office hours... would you recommend adding in NOT NULL values for each of these?
