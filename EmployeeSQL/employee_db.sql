acc264843273951002934a4aebe698e4bcc029c0
dkuchan415 <danielkuchan@yahoo.com>
2020-10-28 20:38:53 -0500

    Completed schema and query database

--- EmployeeSQL/employee_db.sql
+++ EmployeeSQL/employee_db.sql
@@ -3,7 +3,7 @@
 --Create Employee Info table
 
 CREATE TABLE employee_info (
-	emp_no int PRIMARY KEY,
+	emp_no int PRIMARY KEY NOT NULL,
 	emp_title_id varchar(5),
 	birth_date date,
 	first_name varchar(30),
@@ -12,16 +12,12 @@ CREATE TABLE employee_info (
 	hire_date date
 );	
 
---Select from employee table to make sure data is correct
-
 SELECT * FROM employee_info;
---Is there a better way to display sex? (M vs. F) possibly as only accepting the two values.
---Is "date" the right way to identify birthdate and  hiredate?
 
 --Create Employee Salaries table
 
 CREATE TABLE employee_salaries (
-	emp_no int,
+	emp_no int PRIMARY KEY,
 	salary money
 );
 
@@ -29,7 +25,7 @@ SELECT *FROM employee_salaries;
 --Create Employee Titles table 
 
 CREATE TABLE employee_titles (
-	title_id varchar(5),
+	title_id varchar(5) PRIMARY KEY,
 	title varchar(30)
 );
 
@@ -62,4 +58,65 @@ CREATE TABLE manager_department (
 
 SELECT * FROM manager_department;
 
---Questions for office hours... would you recommend adding in NOT NULL values for each of these?
+-- List the following details of each employee: employee number, last name, first name, sex, and salary. (COMPLETE)
+
+SELECT employee_info.emp_no, last_name, first_name, sex, salary FROM
+employee_info
+LEFT JOIN employee_salaries
+ON employee_info.emp_no=employee_salaries.emp_no;
+
+-- List first name, last name, and hire date for employees who were hired in 1986. (COMPLETE)
+
+SELECT first_name, last_name, hire_date FROM employee_info
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+-- (COMPLETE)
+
+SELECT md.dept_no, d.dept_name, ei.emp_no, ei.last_name, ei.first_name FROM employee_info AS ei
+RIGHT JOIN manager_department AS md
+ON ei.emp_no=md.emp_no
+LEFT JOIN department AS d
+ON md.dept_no=d.dept_no;
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+-- (COMPLETE)
+
+SELECT ei.emp_no, ei.last_name, ei.first_name, d.dept_name FROM employee_info AS ei
+LEFT JOIN employee_department AS ed
+ON ei.emp_no=ed.emp_no
+LEFT JOIN department AS d
+ON ed.dept_no=d.dept_no;
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+--(COMPLETE)
+
+SELECT ei.first_name, ei.last_name, ei.sex FROM employee_info AS ei
+WHERE first_name='Hercules' 
+AND last_name LIKE 'B%';
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+--(COMPLETE)
+
+SELECT ei.emp_no, ei.first_name, ei.last_name, d.dept_name FROM employee_info AS ei
+LEFT JOIN employee_department AS ed
+ON ei.emp_no=ed.emp_no
+LEFT JOIN department AS d
+ON ed.dept_no=d.dept_no
+WHERE d.dept_name='Sales';
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+SELECT ei.emp_no, ei.first_name, ei.last_name, d.dept_name FROM employee_info AS ei
+LEFT JOIN employee_department AS ed
+ON ei.emp_no=ed.emp_no
+LEFT JOIN department AS d
+ON ed.dept_no=d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
+
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+--(COMPLETE)
+
+SELECT last_name, COUNT(*) FROM employee_info
+GROUP BY last_name
+ORDER BY COUNT(*) desc;
---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,88 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/IBoxHa
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+-- Create table for employees.csv
+
+CREATE TABLE "employee_info" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" varchar(5)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(30)   NOT NULL,
+    "last_name" varchar(30)   NOT NULL,
+    "sex" string   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employee_info" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+-- Create table for salaries.csv
+CREATE TABLE "employee_salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" money   NOT NULL,
+    CONSTRAINT "pk_employee_salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+-- Create table for titles.csv
+CREATE TABLE "employee_titles" (
+    "title_id" varchar(5)   NOT NULL,
+    "title" varchar(50   NOT NULL,
+    CONSTRAINT "pk_employee_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+-- Create table for dept_emp.csv
+CREATE TABLE "employee_department" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar(4)   NOT NULL,
+    CONSTRAINT "pk_employee_department" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+-- Create table for department
+CREATE TABLE "department" (
+    "dept_no" varchar(4)   NOT NULL,
+    "dept_name" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_department" PRIMARY KEY (
+        "dept_no"
+     ),
+    CONSTRAINT "uc_department_dept_name" UNIQUE (
+        "dept_name"
+    )
+);
+
+-- Create table for dept_manager.csv
+CREATE TABLE "manager_department" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar(4)   NOT NULL,
+    CONSTRAINT "pk_manager_department" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+ALTER TABLE "employee_info" ADD CONSTRAINT "fk_employee_info_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "employee_titles" ("title_id");
+
+ALTER TABLE "employee_salaries" ADD CONSTRAINT "fk_employee_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee_info" ("emp_no");
+
+ALTER TABLE "employee_department" ADD CONSTRAINT "fk_employee_department_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee_info" ("emp_no");
+
+ALTER TABLE "department" ADD CONSTRAINT "fk_department_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "employee_department" ("dept_no");
+
+ALTER TABLE "manager_department" ADD CONSTRAINT "fk_manager_department_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee_info" ("emp_no");
+
+ALTER TABLE "manager_department" ADD CONSTRAINT "fk_manager_department_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "department" ("dept_no");
+
